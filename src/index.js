90e9d046d7eb2a354753d175c2fef191784589c7
valerydluski <valerydluski@gmail.com>
2020-03-04 13:01:54 +0300

    feat: add some function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,41 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    let str = expra.replace( /\s/g, "");
+    let result=0;
+    let str = expr.replace( /\s/g, '');
+    let arr = str.split(/(\D)/);
+    function multipl(a,b){
+        return (+a)*(+b);
+    }
+    function sum(a,b){
+        return (+a)+(+b);
+    }
+    function div(a,b){
+        if (+b===0){
+            throw 'TypeError: Division by zero.';
+        }
+        return (+a)/(+b);
+    }
+    function subtr(a,b){
+        return (+a)-(+b);
+    }
+    for(let i=0; i<arr.length; i++){
+        if (arr[i]==='*') {
+            result+=multipl(arr[i-1],arr[i+1]);
+        }
+        if (arr[i]==='+') {
+            result+=sum(arr[i-1],arr[i+1]);
+        }
+        if (arr[i]==='/') {
+            result+=div(arr[i-1],arr[i+1]);
+        }
+        if (arr[i]==='-') {
+            result+=subtr(arr[i-1],arr[i+1]);
+        }
+    }
+    console.log(result);
+    return result;
+
 }
 
 module.exports = {
