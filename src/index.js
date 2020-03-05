4add07015d9fb253f0d2a992bf28c6d6f06882f9
valerydluski <valerydluski@gmail.com>
2020-03-05 12:59:35 +0300

    fix: fix one bug

--- src/index.js
+++ src/index.js
@@ -19,33 +19,7 @@ function subtr(a,b){
     return (+a)-(+b);
 }
 
-function calc(arr){
-    for(let i=0; i<arr.length; i++){
-        if (arr[i]==='*') {
-            let stack = multipl(arr[i-1],arr[i+1]); 
-            arr.splice(i-1, 3, stack);
-            i-=1;   
-        }
-        if (arr[i]==='/') {
-            let stack = div(arr[i-1],arr[i+1]); 
-            arr.splice(i-1, 3, stack);
-            i-=1;    
-        }
-    } 
-    for(let i=0; i<arr.length; i++){
-        if (arr[i]==='+') {
-            let stack = sum(arr[i-1],arr[i+1]); 
-            arr.splice(i-1, 3, stack);   
-            i-=1;         
-        }
-        if (arr[i]==='-') {
-            let stack = subtr(arr[i-1],arr[i+1]); 
-            arr.splice(i-1, 3, stack); 
-            i-=1;   
-        }
-    }
-    return arr[0]; 
-}
+
 
 function expressionCalculator(expr) {
     let str = expr.replace( /\s/g, '');
@@ -53,40 +27,75 @@ function expressionCalculator(expr) {
 
     let stack = 0;
     let bracketsCount = 0;
-    for(let i=0; i<arr.length; i++){
-        if(arr[i]==='('){
-            bracketsCount++;
+    
+        for(let i=0; i<arr.length; i++){
+            if(arr[i]==='('){
+                bracketsCount++;
+            }
+            if(arr[i]===')'){
+                bracketsCount--;
+            } 
         }
-        if(arr[i]===')'){
-            bracketsCount--;
-        } 
-    }
-    if(bracketsCount != 0){
-        throw 'ExpressionError: Brackets must be paired';   
+        if(bracketsCount != 0){
+            throw 'ExpressionError: Brackets must be paired';   
+        }
+    if(expr.match(/\(/g) != null) { 
+        let stackIndex;
+        for(let i=arr.length-1; i>=0; i--){
+            if (arr[i]==='('){
+            let indexOpen=i;
+                
+                let stackArr=[];
+                for (let k=i+1; k<arr.length; k++){
+                    if (arr[k]==='('){
+                        stackIndex=indexOpen;    
+                        break;
+                    }
+                    if (arr[k]===')'){    
+                    stackArr = arr.slice(i+1,k)
+                    let x = calc(stackArr);
+                    arr.splice(i-1, k-i+3, x);
+                    console.log(stackIndex);
+                    
+                    }
+                
+                }
+            }
+        }
     }
 
-    for(let i=0; i<arr.length; i++){
-        if (arr[i]==='('){
-            let indexOpen=i;
-            let stackArr=[];
-            for (let k=i+1; k<arr.length; k++){
-                if (arr[k]==='('){
-                    break;
-                }
-                if (arr[k]===')'){    
-                   stackArr = arr.slice(indexOpen+1,k)
-                   let x = calc(stackArr);
-                   arr.splice(indexOpen-1, k-indexOpen+3, x); 
-                }  
-            }    
+    function calc(arr){
+        for(let i=0; i<arr.length; i++){
+            if (arr[i]==='*') {
+                let stack = multipl(arr[i-1],arr[i+1]); 
+                arr.splice(i-1, 3, stack);
+                i-=1;   
+            }
+            if (arr[i]==='/') {
+                let stack = div(arr[i-1],arr[i+1]); 
+                arr.splice(i-1, 3, stack);
+                i-=1;    
+            }
+        } 
+        for(let i=0; i<arr.length; i++){
+            if (arr[i]==='+') {
+                let stack = sum(arr[i-1],arr[i+1]); 
+                arr.splice(i-1, 3, stack);   
+                i-=1;         
+            }
+            if (arr[i]==='-') {
+                let stack = subtr(arr[i-1],arr[i+1]); 
+                arr.splice(i-1, 3, stack); 
+                i-=1;   
+            }
         }
+        return arr[0]; 
     }
-     
+    
     console.log(arr);
     let result =+calc(arr);    
     
     return result;
-
 }
 
 module.exports = {
